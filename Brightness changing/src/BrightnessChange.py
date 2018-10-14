90ff5bd64ae41980cc77f95d3fdc3f7614786e66
Shriya Gayatri <35976311+shriyagayatri@users.noreply.github.com>
2018-10-14 18:04:36 +0530

    Update BrightnessChange.py

--- Brightness changing/src/BrightnessChange.py
+++ Brightness changing/src/BrightnessChange.py
@@ -4,6 +4,7 @@ image = Image.open("colourful.jpg")
 
 # image.point creates a new copy of the original image
 
+image.point(lambda x: x*0).save("dark.jpg")
 image.point(lambda x: x*0.1).save("darkest.jpg")
 image.point(lambda x: x*0.5).save("darker.jpg")
 image.point(lambda x: x*1.5).save("bright.jpg")
